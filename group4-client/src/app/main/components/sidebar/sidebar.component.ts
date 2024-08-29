82b7b3b31749a0d9e1c243355e24a27ff7404a73
Hamdan Azani <hamdanpesat@gmail.com>
2024-08-29 11:24:11 +0700

    FEAT:Create sidebar component

---
+++ group4-client/src/app/main/components/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule]
+})
+export class SidebarComponent {
+  isCollapsed = false;
+
+  toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
--- group4-client/src/app/models/customer.model.ts
+++ group4-client/src/app/models/customer.model.ts
@@ -3,4 +3,5 @@ export interface Customer{
   firstName: string
   lastName: string
   phone: string
+  active: boolean
 }
